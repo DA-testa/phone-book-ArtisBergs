addbf3f66d7f7eb6a32d0c4f6531fca0910aa247
ArtisBergs <bergs.artis@gmail.com>
2023-04-02 17:25:53 +0300

    the one and only

--- main.py
+++ main.py
@@ -1,5 +1,46 @@
 # python3
 
+class Phonebook:
+    def __init__(self, bucket_count):
+        self.bucket_count = bucket_count
+        self.buckets = [[] for _ in range(self.bucket_count)]
+        self._multiplier = 31
+        self._prime = 1000000007
+
+    def _hash_func(self, s):
+        """The best hash function from universal family"""
+        ans = 0
+        for c in reversed(str(s)):
+            ans = (ans * self._multiplier + ord(c)) % self._prime
+        return ans % self.bucket_count
+    
+    def add(self, query):
+        string = query.number
+        hashed = self._hash_func(string)
+        bucket = self.buckets[hashed]
+        for q in bucket:
+            if query.number == q.number:
+                q.name = query.name
+                return
+        self.buckets[hashed] = [query] + bucket
+
+    def delete(self, query):
+        string = query.number
+        hashed = self._hash_func(string)
+        bucket = self.buckets[hashed]
+        for i in range(len(bucket)):
+            if bucket[i].number == query.number:
+                bucket.pop(i)
+                break
+
+    def find(self, query):
+        string = query.number
+        hashed = self._hash_func(string)
+        for q in self.buckets[hashed]:
+            if query.number == q.number:
+                return q.name
+        return "not found"
+
 class Query:
     def __init__(self, query):
         self.type = query[0]
@@ -15,33 +56,16 @@ def write_responses(result):
     print('\n'.join(result))
 
 def process_queries(queries):
+    phonebook = Phonebook(9)
     result = []
-    # Keep list of all existing (i.e. not deleted yet) contacts.
-    contacts = []
     for cur_query in queries:
         if cur_query.type == 'add':
-            # if we already have contact with such number,
-            # we should rewrite contact's name
-            for contact in contacts:
-                if contact.number == cur_query.number:
-                    contact.name = cur_query.name
-                    break
-            else: # otherwise, just add it
-                contacts.append(cur_query)
+            phonebook.add(cur_query)
         elif cur_query.type == 'del':
-            for j in range(len(contacts)):
-                if contacts[j].number == cur_query.number:
-                    contacts.pop(j)
-                    break
+            phonebook.delete(cur_query)
         else:
-            response = 'not found'
-            for contact in contacts:
-                if contact.number == cur_query.number:
-                    response = contact.name
-                    break
-            result.append(response)
+            result.append(phonebook.find(cur_query))
     return result
 
 if __name__ == '__main__':
     write_responses(process_queries(read_queries()))
-
